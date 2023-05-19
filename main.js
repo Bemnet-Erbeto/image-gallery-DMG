9f5052f90d6757dc6100d96a113a849b35ebd978
Bemnet Erbeto <bemneterbeto@gmail.com>
2023-05-19 15:26:19 -0400

    In-class demo code

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ function main () {
 }
 
 function populateAlbumDropdown() {
+
+    // The target of a form's "submit" event is the form.
+    // The target of cllick event is the element clicked.
+    // The targent of a change event is the element change. 
     const selectElement = document.querySelector("#album-choice")
-    selectElement.onchange = loadImages
+    selectElement.onchange = handleAlbumChange
 
     for (const album of albums) {
         // Example: <option value="album.folder">album.name</option>
@@ -24,17 +28,31 @@ function populateAlbumDropdown() {
     }
 }
 
-function loadImages (album) {
-    const parentElement = document.querySelector("main")
+function handleAlbumChange (event) {
+    // The "event" object describes the event that occurred.
+    // "event.target" is the element that is the target of the event. in other words, in the case of an "onchange" event, event.target is the element that changed ("<select>"). In the case of a "click" event. target is the elemnet that was clicked.
+    console.log(event.target.value)
+    
+    
+    const selectedAlbumFolder = event.target.value
 
-    for (const path of album.paths) {
+    // This function determines whether a given album is the one the user selected.
+    // WHEN THIS FUNCTION RETURNS true, THE FIND() method will 
+    const albumFolderByUserChoice = album => album.folder === selectedAlbumFolder
+
+    // The find() method will serarch the "albums" array for an album which meets the condition set by albumFolderByUserChoice()
+    const selectedAlbum = albums.find(albumFolderByUserChoice)
+    const parentElement = document.querySelector("main")
+    // replace children with nothing. this will help with the problem of having repet of image.
+    parentElement.replaceChildren()
+    for (const path of selectedAlbum.paths) {
         // VISUALIZE THE GOAL: <img class="card" src="insert-image-path-here">
 
         // 1. Create the empty element by tag name:
         const imageElement = document.createElement("img")
 
         // 2. Customize it in some way:
-        imageElement.setAttribute("src", path)
+        imageElement.setAttribute("src", `images/${selectedAlbumFolder}/${path}`)
         imageElement.classList.add("img-thumbnail", "gallery-image")
 
         // 3. Add it to an existing branch on the DOM tree:
